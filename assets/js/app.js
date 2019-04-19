b02f005f0d941fefa7aac75d889d721d4abd964c
tuyengabi <tuyen.gabi@gmail.com>
2019-04-19 18:21:35 +0200

    v-if v-bind v-show v-for

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,21 @@
-import {sayHi} from './test.js';
-sayHi('jai mon prof');
-require('../stylesheets/main.scss');
+require("../stylesheets/main.scss");
+
+new Vue ({
+  el: ".hello",
+  data: {
+    message: "hello world",
+    paragraph: "toto tutu",
+    show: true,
+    titre: false,
+    fruits : ['pomme','ananas','banane'],
+    styles: {
+      'font-size': "6rem",
+      color: "blue"
+    }
+  },
+  methods: {
+    addMessage: function() {
+
+    }
+  }
+})
--- assets/js/test.js
+++
@@ -1,3 +0,0 @@
-export function sayHi(value) {
-  console.log(value);
-}
